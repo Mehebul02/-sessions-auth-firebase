372371eb483a6c7d6a291fe3d8600619ce46890c
Mehebul02 <mehebul2123@gmail.com>
2024-04-08 13:05:40 +0600

    added

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,13 +1,16 @@
 import { createContext } from "react";
-
+import PropTypes from 'prop-types';
 export const AuthContext = createContext(null)
 const AuthProvider = ({children}) => {
     const authInfo ={name:'mehebul Alif'}
     return (
-       <AuthContext.Provider value={authInfo}>\
+       <AuthContext.Provider value={authInfo}>
        {children}
        </AuthContext.Provider>
     );
 };
 
 export default AuthProvider;
+AuthProvider.propTypes = {
+    children:PropTypes.node
+}
